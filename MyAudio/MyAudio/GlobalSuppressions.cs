f461d53235f620b43682a94b634f8b0b1c6770d1
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-02-23 18:45:25 +0000

    Suppressed "this" rule

--- MyAudio/MyAudio/GlobalSuppressions.cs
+++ MyAudio/MyAudio/GlobalSuppressions.cs
@@ -12,3 +12,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Not required", Scope = "namespace", Target = "~N:MyAudio.Services")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Valid as it is suppression file")]
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements should appear in the correct order", Justification = "Is for a singleton instance.", Scope = "member", Target = "~F:MyAudio.Services.MyAudioDatabase.Instance")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Removed to keep code tidy.", Scope = "member")]
