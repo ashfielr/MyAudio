bfa0644e6078cb99cb0f1c4302620f1e88906c18
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-03-01 15:46:12 +0000

    Added converter to dislpay audio file durations MM:SS

---
+++ MyAudio/MyAudio/Converters/MillisecondsToTimeFormatStr.cs
@@ -0,0 +1,50 @@
+﻿namespace MyAudio.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// Converter class that converts milliseconds to a time string format - MM:SS.
+    /// </summary>
+    internal class MillisecondsToTimeFormatStr : IValueConverter
+    {
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var convertedVal = (int)value;
+            int mins = convertedVal / 1000 / 60;
+            int sec = (convertedVal - (mins * 1000 * 60)) / 1000;
+
+            string minsStr;
+            string secStr;
+
+            if (mins < 10)
+            {
+                minsStr = $"0{mins}";
+            }
+            else
+            {
+                minsStr = mins.ToString();
+            }
+
+            if (sec < 10)
+            {
+                secStr = $"0{sec}";
+            }
+            else
+            {
+                secStr = sec.ToString();
+            }
+
+            return $"{minsStr}:{secStr}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
