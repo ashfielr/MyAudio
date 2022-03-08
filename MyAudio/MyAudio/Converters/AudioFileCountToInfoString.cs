77f1fca63d14e91a9874b1db3352d93de6b269a6
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-03-08 11:32:50 +0000

    Added converter for audio file count info for a playlist

---
+++ MyAudio/MyAudio/Converters/AudioFileCountToInfoString.cs
@@ -0,0 +1,25 @@
+﻿namespace MyAudio.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// Converts an integer number of audio file into an info string used when displaying playlists.
+    /// </summary>
+    internal class AudioFileCountToInfoString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var convertValue = (int)value;
+            return $"{convertValue} Audio File(s)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
