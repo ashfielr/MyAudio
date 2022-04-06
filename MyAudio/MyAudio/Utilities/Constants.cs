38efc6ca7ba1b3a8423d57586eb6c10b2a2876db
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-04-06 11:05:11 +0100

    removed old constants for sqlite database usage (no longer needed)

--- MyAudio/MyAudio/Utilities/Constants.cs
+++ MyAudio/MyAudio/Utilities/Constants.cs
@@ -10,39 +10,8 @@
     /// </summary>
     internal static class Constants
     {
-        /// <summary>
-        /// Name of database file.
-        /// </summary>
-        public const string DatabaseFilename = "myAudioSQLite.db3";
-
         public const string FirebaseProjectID = "myaudio-xamarin-app-dd5da";
 
         public const string FirebaseStorageURL = "myaudio-xamarin-app-dd5da.appspot.com";
-
-        /// <summary>
-        /// The modes of opening access to database.
-        /// </summary>
-        public const SQLite.SQLiteOpenFlags Flags =
-
-            // open the database in read/write mode
-            SQLite.SQLiteOpenFlags.ReadWrite |
-
-            // create the database if it doesn't exist
-            SQLite.SQLiteOpenFlags.Create |
-
-            // enable multi-threaded database access
-            SQLite.SQLiteOpenFlags.SharedCache;
-
-        /// <summary>
-        /// Gets the databse path.
-        /// </summary>
-        public static string DatabasePath
-        {
-            get
-            {
-                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                return Path.Combine(basePath, DatabaseFilename);
-            }
-        }
     }
 }
